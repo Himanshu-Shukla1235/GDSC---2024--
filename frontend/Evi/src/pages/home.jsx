23d9166b480c2ecb76c8880a5f2fbbfcc6a44534
Blurix23 <145251086+Blurix23@users.noreply.github.com>
2024-02-03 16:08:16 +0530

    home changes

--- frontend/Evi/src/pages/home.jsx
+++ frontend/Evi/src/pages/home.jsx
@@ -4,7 +4,7 @@ import "../pages/home.css";
 import { Fade } from "react-slideshow-image";
 import { blue } from "@mui/material/colors";
 import Nav from "../components/Nav";
-
+// import { FontAwesomeIcon } from "react-icons/fa";
 const Home = () => {
 
 
@@ -12,12 +12,12 @@ const Home = () => {
 
   return (
     <>
-      <header>  <Nav></Nav></header>
+      <header className="header1">  <Nav></Nav></header>
       <main className="main2">
         <div className="boxH">
        
         </div>
-        <div><div></div></div>
+        <div className="boxH3"><div className="boxH31"></div></div>
         <div className="boxH1"></div>
       </main>
       
