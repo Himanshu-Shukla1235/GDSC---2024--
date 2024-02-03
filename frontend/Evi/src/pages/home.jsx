8a88c659f2f4ed5f810d186e31320d2bfa3b5941
amgubrele <amgubrele011@gmail.com>
2024-02-03 16:13:43 +0530

    f

--- frontend/Evi/src/pages/home.jsx
+++ frontend/Evi/src/pages/home.jsx
@@ -22,6 +22,7 @@ const Home = () => {
         <div><div></div></div>
         <div className="boxH1"></div>
       </main>
+      <footer className="footer"><Footer></Footer></footer>
       
     </>
   );
