2e87054fc98f595421c8c6abcddd262dea781ed6
amgubrele <amgubrele011@gmail.com>
2024-02-04 19:47:40 +0530

    login

--- frontend/Evi/src/pages/login.jsx
+++ frontend/Evi/src/pages/login.jsx
@@ -3,65 +3,59 @@ import './Login.css'; // Import your CSS file
 import Navbar from '../components/Nav';
 
 const AuthForm = () => {
-  const [isSignUp, setIsSignUp] = useState(false);
+  const [login,setLogin]=useState(true);
+  const [header,setHeader]=useState('Login');
+  const [option,setOption]=useState('register')
 
-  const handleToggleForm = () => {
-    setIsSignUp((prev) => !prev);
-  };
+  const setSignUp=()=>{
+   if(login==true)
+   {
+     setHeader('Register')
+    setLogin(false);
+    setOption('login');
+   }
+   else{
+    setHeader('Login')
+    setLogin(true);
+    setOption('register');
+   }
+  }
+
+
+ 
 
   return (
-    <>
-    <header className='header1'><Navbar></Navbar></header>
-    <div className='inputBody'>
-    <div  className={`container ${isSignUp ? 'right-panel-active' : ''}`}>
-      <div className="form-container sign-up-container">
-        <form  className="inputForm" action="#">
-          <h1>Create Account</h1>
-          <div className="social-container">
-            <a href="#" className="social inputA"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="social inputA"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="social inputA"><i className="fab fa-linkedin-in"></i></a>
-          </div>
-          <span className='inputSpan'>or use your email for registration</span>
-          <input className="loginInput"type="text" placeholder="Name" />
-          <input className="loginInput"type="email" placeholder="Email" />
-          <input className="loginInput"type="password" placeholder="Password" />
-          <button className='inputButton' >Sign Up</button>
+  <>
+    <header className='header1'>
+      <Navbar></Navbar>
+    </header>
+    <div className="loginBody">
+      <div className="loginContainer">
+        <h2 className='loginHeading' >{header}</h2>
+      <div className="inner">
+        <form className='loginForm' id="form">
+          <input className="loginInput"id="username" type="text" placeholder="Username" />
+          {!login && (
+                // Additional input fields for the 'regiseter' state
+                <>
+                  
+                  <input className="loginInput" id="loginAdditionalField" type="text" placeholder="email" />
+                </>
+              )}
+          <input className="loginInput" id="password" type="password" placeholder="Password" />
+          <button className='loginButton' type="submit">
+            Submit<i id="loader" className="fa-solid fa-spinner fa-spin-pulse"></i>
+          </button>
+
+          <a className='loginRegister' onClick={setSignUp}>{option}</a>
         </form>
-      </div>
-      <div className="form-container sign-in-container">
-        <form className="inputForm" action="#">
-          <h1>Sign in</h1>
-          <div className="social-container">
-            <a href="#" className="social inputA"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="social inputA"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="social inputA"><i className="fab fa-linkedin-in"></i></a>
-          </div>
-          <span className='inputSpan'>or use your account</span>
-          <input className="loginInput"type="email" placeholder="Email" />
-          <input className="loginInput"type="password" placeholder="Password" />
-          <a className='inputA'href="#">Forgot your password?</a>
-          <button className='inputButton'>Sign In</button>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className={`overlay-panel overlay-left ${isSignUp ? 'overlay-left-active' : ''}`}>
-            <h1>Welcome Back!</h1>
-            <p className='inputP'>To keep connected with us please login with your personal info</p>
-            <button className="ghost inputButton" onClick={handleToggleForm}>Sign In</button>
-          </div>
-          <div className={`overlay-panel overlay-right ${isSignUp ? 'overlay-right-active' : ''}`}>
-            <h1>Hello, Friend!</h1>
-            <p className='inputP'>Enter your personal details and start the journey with us</p>
-            <button className="ghost inputButton" onClick={handleToggleForm}>Sign Up</button>
-          </div>
-        </div>
+        <p className="secretid"></p>
       </div>
     </div>
     </div>
-    </>
-  );
+  </>
+);
+
 };
 
 export default AuthForm;
